bd8735a8139f414bec15097effe8955630b9767f
Nick Weeden <nicholas_weeden@alumni.brown.edu>
2020-09-30 09:06:22 -0700

    centered board

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <div className = 'container'>
           <div className = 'welcome'>
             <h1>Welcome to <strong>Lights Out</strong></h1>
-            <h5><i>Lights Out is a puzzle, played on a grid of individual lights, which can either be lit or unlit. The puzzle is won when when all of the lights are turned off. You can click on a cell to toggle that light — but it also toggles the light above, to the left, to the right, and below it.</i></h5>
+            <h5><i>Lights Out is a puzzle, played on a grid of lights, which can each either be lit or unlit. The puzzle is won when when all of the lights are turned off. You can click on a cell to toggle that light — but it also toggles the light above, to the left, to the right, and below it.</i></h5>
             <br></br>
             <h5><strong>Good Luck!</strong></h5>
             <br></br>
